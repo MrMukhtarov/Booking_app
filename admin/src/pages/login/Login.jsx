0407304fb933d8c62229df919881dea9f108cc1f
MrMukhtarov <101628108+MrMukhtarov@users.noreply.github.com>
2023-03-15 13:19:51 +0400

    first commit

---
+++ admin/src/pages/login/Login.jsx
@@ -0,0 +1,68 @@
+import { useContext } from "react";
+import { useState } from "react";
+import "./login.scss";
+// import { AuthContext } from "../../context/AuthContext.js";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+
+const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
+
+  const { loading, error, dispatch } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", credentials);
+      if (res.data.isAdmin) {
+        dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+        navigate("/");
+      } else {
+        dispatch({
+          type: "LOGIN_FAILIER",
+          payload: { message: "You are not allowed" },
+        });
+      }
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILIER", payload: err.response.data });
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="lContainer">
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          onChange={handleChange}
+          className="lInput"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          onChange={handleChange}
+          className="lInput"
+        />
+        <button disabled={loading} onClick={handleClick} className="lButton">
+          Login
+        </button>
+        {error && <spam>{error.message}</spam>}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
